20f2bde8f4ee5fa56b2310bf4e04cd0d394467e2
Abhishek Ahuja <abhishek.ah@prod.hclpnp.com>
2022-08-21 12:49:10 +0530

    first commit

---
+++ bms-server/src/test/java/com/mybms/bms/BmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybms.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
